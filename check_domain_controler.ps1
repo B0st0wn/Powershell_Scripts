afd85300f7fa0379fdb24d86e0adc8a69c2dc122
B0st0wn <39604785+B0st0wn@users.noreply.github.com>
2023-06-17 13:11:08 -0400

    Made the script more robust and made it more customizable.

--- check_domain_controler.ps1
+++ check_domain_controler.ps1
@@ -1,1 +1,14 @@
-﻿Get-ItemProperty 'Microsoft.PowerShell.Core\Registry::HKEY_CURRENT_USER\Volatile Environment\' | Select-Object LogonServer
+﻿$desiredLogonServer = "\\HAN-DC2-FILE"
+$regPath = 'HKCU:\Volatile Environment'
+$logonServerValue = 'LogonServer'
+
+$currentLogonServer = Get-ItemPropertyValue -Path $regPath -Name $logonServerValue
+
+if ($currentLogonServer -ne $desiredLogonServer) {
+    Set-ItemProperty -Path $regPath -Name $logonServerValue -Value $desiredLogonServer
+    Write-Host "Logon server has been updated. Please log off and log back on for the changes to take effect."
+}
+else {
+    Write-Host "The logon server is already set to the desired value."
+}
+
