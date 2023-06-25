9fa5324188ff24dd4844c2b8f3bd928ee2670893
B0st0wn <39604785+B0st0wn@users.noreply.github.com>
2023-06-25 17:22:35 -0400

    Changed zip action from windows zip archive to 7zip to be able to add to the zip file. Also changed it to 7zip format for better compression.

--- Azure_ADSync_Check.ps1
+++ Azure_ADSync_Check.ps1
@@ -1,66 +1,45 @@
-<#
-Editor: Visual Studio Code
-Script Name: Test.ps1
-Author: Paul Hanlon
-Date: 6/16/2023
-Description: 
-  This PowerShell script checks if the service named "ADSync" is running. 
-  If the service is not found, an error is thrown and logged. 
-  If the service is found but not running, it starts the service and logs the event.
-  The script also creates a log file. If a log file already exists, it zips the old log file and creates a new one. 
-  If a zip file does not exist, it creates one.
-#>
+$serviceName = "ADSync"
 
-$serviceName = "ADSync" # Replace with the correct service name if different
-
-# Create the folder if it doesn't exist
-$folderPath = "$env:PUBLIC\Desktop\Tasks Log"
+# Change to network share path
+$folderPath = "\\NAS\Systems\Azure\Azure_Logs"
 if (!(Test-Path -Path $folderPath)){
     New-Item -ItemType Directory -Force -Path $folderPath
 }
 
-# Set the path for the log file, with the current time in the filename
-$logFileName = "{0}_Check_{1:HHmmss}.txt" -f $serviceName, (Get-Date)
+$logFileName = "{0}_Check_{1:HHmmss}.log" -f $serviceName, (Get-Date)
 $logPath = Join-Path -Path $folderPath -ChildPath $logFileName
 
-# Create the log file
 $null = New-Item -Path $logPath -ItemType File -Force
 
-# Redirect verbose and error messages to the log file
 Start-Transcript -Path $logPath -Force
 
-# Check if the service exists and is not running, then start it
 try {
     $service = Get-Service -Name $serviceName -ErrorAction Stop
     if ($service.Status -eq 'Stopped') {
         Start-Service -Name $serviceName -ErrorAction Stop
-        Write-Host "The '$serviceName' service has been started." # write to the console and log file
+        Write-Host "The '$serviceName' service has been started."
     }
     else {
         Write-Host "The '$serviceName' service is already running."
     }
 } catch {
-    # Log any error messages
     Write-Host "Failed to ensure that '$serviceName' is running. Error: $_"
     exit 1
 }
 
-# Compress old logs
-$zipPath = Join-Path -Path $folderPath -ChildPath "old_logs.zip"
-$oldLogs = Get-ChildItem -Path $folderPath -Filter "*.txt" | Where-Object { $_.FullName -ne $logPath }
+$zipPath = Join-Path -Path $folderPath -ChildPath "old_logs.7z"
+$oldLogs = Get-ChildItem -Path $folderPath -Filter "*.log" | Where-Object { $_.FullName -ne $logPath }
+
+# Add files to 7z archive using 7-zip
 if ($oldLogs.Count -gt 0) {
-    if (!(Test-Path -Path $zipPath)) {
-        [System.IO.Compression.ZipFile]::CreateFromDirectory($folderPath, $zipPath, 'Optimal', $false)
-    } else {
-        $zipFile = [System.IO.Compression.ZipFile]::Open($zipPath, 'Update')
-        foreach ($oldLog in $oldLogs) {
-            $zipEntry = $zipFile.CreateEntryFromFile($oldLog.FullName, $oldLog.Name)
-            $zipEntry.LastWriteTime = $oldLog.LastWriteTime
+    foreach ($log in $oldLogs) {
+        $7zipResult = Start-Process -FilePath "C:\Program Files\7-Zip\7z.exe" -ArgumentList "a -t7z -mx9 `"$zipPath`" `"$($log.FullName)`"" -Wait -PassThru
+        if ($7zipResult.ExitCode -ne 0) {
+            Write-Host "7-zip encountered an error: exit code $($7zipResult.ExitCode)"
+        } else {
+            Remove-Item -Path $log.FullName -Force
         }
-        $zipFile.Dispose()
     }
-    $oldLogs | Remove-Item -Force
 }
 
-# Stop transcript logging and close the log file
 Stop-Transcript
