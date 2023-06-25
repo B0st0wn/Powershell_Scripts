31fae6dff428940f2f46da457c50de61dfd7cb91
B0st0wn <39604785+B0st0wn@users.noreply.github.com>
2023-06-25 17:36:33 -0400

    change Get-WmiObject to Get-CimInstance bad help doc info.

--- Check Memory.ps1
+++ Check Memory.ps1
@@ -1,8 +1,4 @@
-﻿# Remove the PCname since it's no longer required.
-# Instead, get the local machine's name
-$PCname = $env:COMPUTERNAME
-
-# Load the System.Windows.Forms assembly
+﻿# Load the System.Windows.Forms assembly
 [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")>$null
 
 # Set a flag to repeat the script
@@ -12,9 +8,9 @@ $Repeat = $True
 While ($Repeat)
 {
     # Define a function to retrieve the memory usage of the local computer
-    function Get-MemoryUsage ($ComputerName=$PCname){
+    function Get-MemoryUsage {
         # Retrieve the operating system information from the local computer
-        $ComputerSystem = Get-WmiObject -ComputerName $ComputerName -Class Win32_operatingsystem -Property CSName, TotalVisibleMemorySize, FreePhysicalMemory
+        $ComputerSystem = Get-CimInstance -ClassName Win32_operatingsystem -Property CSName, TotalVisibleMemorySize, FreePhysicalMemory
 
         # Extract the computer name, free memory, and total memory information from the retrieved object
         $MachineName = $ComputerSystem.CSName
