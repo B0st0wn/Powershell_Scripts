3a4f17c0b69edfcd3c45dd5b24a56900c1ca0d58
B0st0wn <39604785+B0st0wn@users.noreply.github.com>
2023-06-25 17:30:06 -0400

    Change the script to run local on the PC and to refresh after 3 minutes the memory results.

--- Check Memory.ps1
+++ Check Memory.ps1
@@ -1,42 +1,38 @@
-﻿# Prompt the user to enter a computer name or IP address
-$PCname = Read-Host "Please enter a computer name or IP"
+﻿# Remove the PCname since it's no longer required.
+# Instead, get the local machine's name
+$PCname = $env:COMPUTERNAME
+
 # Load the System.Windows.Forms assembly
 [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")>$null
+
 # Set a flag to repeat the script
 $Repeat = $True
+
 # Loop while the flag is true
 While ($Repeat)
 {
-    # Define a function to retrieve the memory usage of a specified computer
+    # Define a function to retrieve the memory usage of the local computer
     function Get-MemoryUsage ($ComputerName=$PCname){
-        # Test if the specified computer is online and responding to ping
-        if (Test-Connection -ComputerName $ComputerName -Count 1 -Quiet) {
-            # Retrieve the operating system information from the specified computer
-            $ComputerSystem = Get-WmiObject -ComputerName $ComputerName -Class Win32_operatingsystem -Property CSName, TotalVisibleMemorySize, FreePhysicalMemory
-            # Extract the computer name, free memory, and total memory information from the retrieved object
-            $MachineName = $ComputerSystem.CSName
-            $FreePhysicalMemory = ($ComputerSystem.FreePhysicalMemory) / (1mb)
-            $TotalVisibleMemorySize = ($ComputerSystem.TotalVisibleMemorySize) / (1mb)
-            $TotalVisibleMemorySizeR = "{0:N2}" -f $TotalVisibleMemorySize
-            $TotalFreeMemPerc = ($FreePhysicalMemory/$TotalVisibleMemorySize)*100
-            $TotalFreeMemPercR = "{0:N2}" -f $TotalFreeMemPerc
+        # Retrieve the operating system information from the local computer
+        $ComputerSystem = Get-WmiObject -ComputerName $ComputerName -Class Win32_operatingsystem -Property CSName, TotalVisibleMemorySize, FreePhysicalMemory
 
-            # Display the computer name, total memory, and percentage of free memory
-            "Name: $MachineName"
-            "RAM: $TotalVisibleMemorySizeR GB"
-            "Free Physical Memory: $TotalFreeMemPercR %"
-        }
+        # Extract the computer name, free memory, and total memory information from the retrieved object
+        $MachineName = $ComputerSystem.CSName
+        $FreePhysicalMemory = ($ComputerSystem.FreePhysicalMemory) / (1mb)
+        $TotalVisibleMemorySize = ($ComputerSystem.TotalVisibleMemorySize) / (1mb)
+        $TotalVisibleMemorySizeR = "{0:N2}" -f $TotalVisibleMemorySize
+        $TotalFreeMemPerc = ($FreePhysicalMemory/$TotalVisibleMemorySize)*100
+        $TotalFreeMemPercR = "{0:N2}" -f $TotalFreeMemPerc
+
+        # Display the computer name, total memory, and percentage of free memory
+        "Name: $MachineName"
+        "RAM: $TotalVisibleMemorySizeR GB"
+        "Free Physical Memory: $TotalFreeMemPercR %"
     }
 
     # Call the Get-MemoryUsage function
     Get-MemoryUsage
-    # Prompt the user to run the script again
-    $Answer = [Windows.Forms.MessageBox]::Show("Run Again?", "Repeat",
-        [Windows.Forms.MessageBoxButtons]::YESNO,
-        [Windows.Forms.MessageBoxIcon]::Information)
-    # If the user clicks "No", set the flag to false to exit the loop
-    If ($Answer -eq "No")
-    {
-        $Repeat = $False
-    }
+
+    # Instead of using a message box to prompt the user, we simply pause for 3 minutes (180 seconds)
+    Start-Sleep -Seconds 180
 }
