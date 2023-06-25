7772a55f13ca73422bbb65c0d37a27ca35965a4d
B0st0wn <39604785+B0st0wn@users.noreply.github.com>
2023-06-25 15:57:36 -0400

    This is a commit message

--- Rename Pc.ps1
+++ Rename Pc.ps1
@@ -7,17 +7,18 @@ $computers = Import-Csv -Path "C:\path\to\your\file.csv"
 Rename computers
 foreach ($computer in $computers) {
 
-# Check if new name is valid
-if([string]::IsNullOrEmpty($computer.NewName)) {
-    Write-Error "Invalid new name for computer $computer.OldName. Skipping."
-    continue
-}
+    # Check if new name is valid
+    if ([string]::IsNullOrEmpty($computer.NewName)) {
+        Write-Error "Invalid new name for computer $computer.OldName. Skipping."
+        continue
+    }
 
-# Rename computer
-try {
-    Rename-Computer -ComputerName $computer.OldName -NewName $computer.NewName -DomainCredential $domaincredential -Force -Restart
-    Write-Information "Successfully renamed $computer.OldName to $computer.NewName."
-}
-catch {
-    Write-Error "Failed to rename $computer.OldName. Error: $_"
+    # Rename computer
+    try {
+        Rename-Computer -ComputerName $computer.OldName -NewName $computer.NewName -DomainCredential $domaincredential -Force -Restart
+        Write-Information "Successfully renamed $computer.OldName to $computer.NewName."
+    }
+    catch {
+        Write-Error "Failed to rename $computer.OldName. Error: $_"
+    }
 }
