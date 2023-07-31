07a89c5c404ad76bd5ff0afbc636263503327c16
Paul <paul_hanlon@outlook.com>
2023-07-31 10:20:37 -0400

    modified:   .gitignore 	deleted:    ClamAV_Scan.ps1 	deleted:    test3.ps1

--- ClamAV_Scan.ps1
+++
@@ -1,9 +0,0 @@
-$driveLetters = 'U:', 'W:', 'P:', 'F:'
-
-foreach ($drive in $driveLetters) {
-    if (Test-Path $drive) {
-        Remove-PSDrive -Name $drive.TrimEnd(':') -PSProvider 'FileSystem' -ErrorAction SilentlyContinue
-    } else {
-        Write-Output "Drive $drive does not exist"
-    }
-}
